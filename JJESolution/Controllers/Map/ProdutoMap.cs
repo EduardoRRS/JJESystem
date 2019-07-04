dd376116f949bed7d1de07f1a1efd5f5d763170d
Eduardo Rodrigues <eduardosergiorodrigues@jnmoura.com.br>
2019-07-04 11:53:01 -0300

    Base do projeto

---
+++ JJESolution/Controllers/Map/ProdutoMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+
+namespace Controllers.Map
+{
+    class ProdutoMap : EntityTypeConfiguration<Produto>
+    {
+        public ProdutoMap()
+        {
+            this.ToTable("Produto");
+            this.HasKey(q => q.Id);
+            this.Property(q => q.Nome).HasColumnName("Nome Produto");
+        }
+    }
+}
